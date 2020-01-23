8bc2c5e5a2ae005f72fb633c499df6217f637631
Christian Esteves <c.esteves92@gmail.com>
2020-01-23 13:02:04 -0500

    main file setup.

---
+++ Print Names/Print Names/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/* 
+ * Assignment: Create a new file named '{your_name}.c'
+ * In this file, create a void method to print your name using the 'printf' function.
+ * Push this change to your branch.
+ */
+
+int main() {
+	// Add function call below this line.
+}
